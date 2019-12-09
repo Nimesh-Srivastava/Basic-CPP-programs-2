cf38d158a9dc8ce7ce816a668bb5ef527ef300b1
Nimesh Srivastava <30381993+Nimesh-Srivastava@users.noreply.github.com>
2019-12-09 16:32:59 +0530

    Create greatest_without_logical.cpp

---
+++ greatest_without_logical.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+	system("cls");
+	int n1,n2;
+	cout<<"Enter number 1 : ";
+	cin>>n1;
+	cout<<"Enter number 2 : ";
+	cin>>n2;
+	cout << "Greater number is : " << ((n1 + n2) + (n1 - n2)) / 2;
+  (void)_getch();
+  return 0;
+}
