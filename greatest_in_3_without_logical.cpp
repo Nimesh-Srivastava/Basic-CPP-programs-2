a4731351f915c7359d7ad0d66245423881cdb660
Nimesh Srivastava <30381993+Nimesh-Srivastava@users.noreply.github.com>
2019-12-09 16:35:53 +0530

    Update greatest_in_3_without_logical.cpp

--- greatest_in_3_without_logical.cpp
+++ greatest_in_3_without_logical.cpp
@@ -3,13 +3,33 @@
 using namespace std;
 int main()
 {
-	system("cls");
-	int n1,n2;
-	cout<<"Enter number 1 : ";
-	cin>>n1;
-	cout<<"Enter number 2 : ";
-	cin>>n2;
-	cout << "Greater number is : " << ((n1 + n2) + (n1 - n2)) / 2;
+int a, b, c;
+	cout << "Enter number 1 : ";
+	cin >> a;
+	cout << "Enter number 2 : ";
+	cin >> b;
+	cout << "Enter number 3 : ";
+	cin >> c;
+// 	if((a-b)>0)
+// 	{
+// 		if((a-c))>0
+// 		{
+// 			cout<<"Greatest : "<<a;
+// 		}
+// 		else if((c-b)>0)
+// 		{
+// 			cout<<"Greatest : "<<c;
+// 		}
+// 		else
+// 		{
+// 			cout<<"Greatest : "<<b;
+// 		}
+// 	}
+	int c1 = ((a + b) - (a - b)) / 2;
+	int c2 = ((c1 + c) - (c1 - c)) / 2;
+	int c3 = ((a + b) + (a - b)) / 2;
+	int c4 = ((c3 + c) + (c3 - c)) / 2;
+	cout << "Highest : " << c2 << " > " << c1 << " > " << c4 << " : Lowest" <<endl;
   (void)_getch();
   return 0;
 }
