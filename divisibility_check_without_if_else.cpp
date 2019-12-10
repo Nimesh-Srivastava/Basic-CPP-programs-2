88ab5aee3fea3be38bd4d8a98d1d17cece5dbd88
Nimesh Srivastava <30381993+Nimesh-Srivastava@users.noreply.github.com>
2019-12-10 11:03:48 +0530

    Create divisibility_check_without_if_else.cpp

---
+++ divisibility_check_without_if_else.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+
+	system("cls");
+	
+        int a;
+	cout << "Enter a number : ";
+	cin >> a;
+	a / 55 ? cout << "Number is divisible by both 5 and 11" << endl : cout << "Number is not divisible by both 5 and 11";
+        (void)_getch();
+        return 0;
+}
