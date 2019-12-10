12d5e12d5ae58add8f4c77a466ee1b42464bae06
Nimesh Srivastava <30381993+Nimesh-Srivastava@users.noreply.github.com>
2019-12-10 10:58:27 +0530

    Update compare_without_logical.cpp

--- compare_without_logical.cpp
+++ compare_without_logical.cpp
@@ -1,1 +1,24 @@
+#include<iostream>
+#include<conio.h>
 
+using namespace std;
+
+int main ()
+{
+int a, b;
+	cout << "Enter number 1 : " << endl;
+	cin >> a;
+	cout << "Enter number 2 : " << endl;
+	cin >> b;
+	if (a^b)
+	{
+		cout << "Numbers are not same";
+	}
+	else
+	{
+		cout << "Numbers are same";
+	}
+
+(void)_getch();
+return 0;
+}
